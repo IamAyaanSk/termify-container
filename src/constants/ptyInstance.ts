ba7dad4c6bacb3832adb97a31ec5e62d9d31d520
Ayaan Shaikh <63299939+IamAyaanSk@users.noreply.github.com>
2024-09-12 06:10:11 +0530

    fix: modified terminal height

--- src/constants/ptyInstance.ts
+++ src/constants/ptyInstance.ts
@@ -7,7 +7,7 @@ const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
 const ptyProcessInstance = pty.spawn(shell, [], {
   name: 'xterm-color',
   cols: 80,
-  rows: 15,
+  rows: 12,
   cwd: USER_CODE_BASE_PATH,
   env: {},
 });
